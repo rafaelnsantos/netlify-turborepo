170d893d587200cea8f942179df2270165f054df
Rafael Santos <rafaelnsantos1@gmail.com>
2022-06-29 23:51:53 -0300

    eslint

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,5 +1,6 @@
+/** @type {import('eslint').Linter.BaseConfig} */
 module.exports = {
-  extends: ["next", "prettier"],
+  extends: ["next/core-web-vitals", "plugin:prettier/recommended"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
