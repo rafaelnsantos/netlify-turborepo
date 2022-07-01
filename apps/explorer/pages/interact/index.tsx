5ad163817d8eb6e7e675d3d44a171046cfdd4c48
Rafael Santos <rafaelnsantos1@gmail.com>
2022-07-01 18:41:12 -0300

    add interact dark theme

--- apps/explorer/pages/interact/index.tsx
+++ apps/explorer/pages/interact/index.tsx
@@ -3,9 +3,11 @@ import { useRef } from "react"
 import Editor from "@monaco-editor/react"
 import { NextPageWithLayout, Page } from "ui"
 import { Layout } from "@/components/Layout"
+import { useTheme } from "next-themes"
 
 const InteractIndex: NextPageWithLayout = () => {
   const editorRef = useRef<any>(null)
+  const { theme } = useTheme()
 
   const getCode = () => editorRef.current?.getValue()
 
@@ -21,11 +23,12 @@ const InteractIndex: NextPageWithLayout = () => {
     <Page title="Interact" className="flex flex-col space-y-5">
       <h1 className="text-2xl">Interact</h1>
       <Editor
-        className="border-2 p-1"
+        className="border-2 p-1 border-base-200"
         height="70vh"
         defaultLanguage="kind"
         defaultValue="// some comment"
         onMount={(editor) => (editorRef.current = editor)}
+        theme={theme === "dark" ? "vs-dark" : "light"}
       />
       <div className="space-x-5">
         <button className="btn btn-ghost" onClick={handleTest}>
