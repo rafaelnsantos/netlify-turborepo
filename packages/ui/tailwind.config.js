7a2d82c72ef8685f2144762acca62c62f6a531a2
Rafael Santos <rafaelnsantos1@gmail.com>
2022-06-30 21:06:38 -0300

    themes

--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -1,3 +1,5 @@
+const height = require("./tailwind/height")
+
 /** @type {import('tailwindcss/tailwind-config').TailwindConfig} */
 module.exports = {
   content: [
@@ -5,17 +7,31 @@ module.exports = {
     "./components/**/*.{ts,tsx}",
     "../../packages/ui/**/*.{ts,tsx}",
   ],
+  theme: {
+    extend: {
+      height,
+      minHeight: height,
+    },
+  },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
     themes: [
       {
         light: {
-          ...require("daisyui/src/colors/themes")["[data-theme=light]"],
+          primary: "#570df8",
+          secondary: "#f000b8",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
         },
       },
       {
         dark: {
-          ...require("daisyui/src/colors/themes")["[data-theme=night]"],
+          primary: "#661AE6",
+          secondary: "#D926AA",
+          accent: "#1FB2A5",
+          neutral: "#191D24",
+          "base-100": "#2A303C",
         },
       },
     ],
