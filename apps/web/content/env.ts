7f2d0d7c3389303713f3dae99a705cadeb414d76
Rafael Santos <rafaelnsantos1@gmail.com>
2022-06-30 23:43:25 -0300

    add env

---
+++ apps/web/content/env.ts
@@ -0,0 +1,7 @@
+export const DOCS_URL =
+  process.env.NEXT_PUBLIC_DOCS_URL ?? "//teste-docs.netlify.app"
+
+export const NODES = (
+  process.env.NEXT_PUBLIC_KINDELIA_DEFAULT_NODES ||
+  "143.244.179.61,164.92.214.78,159.223.39.129"
+).split(",")
